9fa90075c9974bb88bb1a88d94b69853a09881de
Christopher Hughes <bekreth@proton.me>
2024-01-04 20:39:10 -0800

    Add ability to store price information
    * Most of the pricing has been added
    
    * Minor state updates.

--- src/views/lists/components/EditItem.tsx
+++ src/views/lists/components/EditItem.tsx
@@ -1,12 +1,15 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { TextField } from "@mui/material"
 import Box from "@mui/material/Box"
 import Modal from "@mui/material/Modal"
 import Typography from "@mui/material/Typography"
+import Select, { SelectChangeEvent }from "@mui/material/Select"
+import MenuItem from "@mui/material/MenuItem"
 import { Button } from "@mui/material"
+import CurrencyInput from "react-currency-input-field"
 
-import { Item } from "database/list"
+import { Item, Price, PriceUnit } from "database/list"
 
 const style = {
 	position: "absolute" as const,
@@ -20,6 +23,10 @@ const style = {
 	p: 4,
 }
 
+const emptyPrice: Price = {
+	amount: undefined,
+	unit: PriceUnit.NONE,
+}
 
 export interface EditItemProps {
   item: Item
@@ -32,7 +39,12 @@ export default function EditItem(props: EditItemProps) {
 	const {item, saveItemEdits, isOpen, close} = props
 
 	const [itemName, setItemName] = useState(item.name)
-	const [itemState] = useState(item.state)
+	const [itemPrice, setItemPrice] = useState<Price>(item.price ? item.price : emptyPrice)
+
+	useEffect(() => {
+		setItemName(item.name)
+		setItemPrice(item.price ? item.price : emptyPrice)
+	},[item])
 
 	return (
 		<Modal
@@ -41,7 +53,7 @@ export default function EditItem(props: EditItemProps) {
 		>
 			<Box sx={style}>
 				<Typography id="modal-modal-title" variant="h5">
-					Editing Item : {item.name}
+					Editing Item : {itemName}
 				</Typography>
 				<br/>
 				<TextField
@@ -52,18 +64,51 @@ export default function EditItem(props: EditItemProps) {
 					onChange={event => setItemName(event.target.value)}
 				/>
 				<br/>
+				<CurrencyInput
+					id="itemCost2"
+					name="itemCostName"
+					prefix="$"
+					defaultValue={item.price ? item.price.amount : undefined}
+					allowNegativeValue={false}
+					placeholder="Item Cost"
+					decimalsLimit={2}
+					onValueChange={(value, name, values) => {
+						if (values) {
+							setItemPrice({
+								...itemPrice,
+								amount: values.float ? values.float : undefined,
+							})
+						}
+					}}
+				/>
+				<Typography>per</Typography>
+				<Select 
+					label="per unit" 
+					defaultValue={item.price ? item.price.unit : PriceUnit.NONE}
+					onChange={(event: SelectChangeEvent<PriceUnit>) => {
+						setItemPrice({
+							...itemPrice,
+							unit: event.target.value as unknown as PriceUnit,
+						})
+					}}
+				>
+					<MenuItem value={PriceUnit.NONE}>{PriceUnit.NONE}</MenuItem>
+					<MenuItem value={PriceUnit.BOX}>{PriceUnit.BOX}</MenuItem>
+					<MenuItem value={PriceUnit.GRAMS_100}>{PriceUnit.GRAMS_100}</MenuItem>
+					<MenuItem value={PriceUnit.LITER}>{PriceUnit.LITER}</MenuItem>
+					<MenuItem value={PriceUnit.POUND}>{PriceUnit.POUND}</MenuItem>
+				</Select>
+				<br/>
 				<Button
 					variant="contained"
 					onClick={() => {
 						const newItem: Item = {
-							_id: item._id,
-							_rev: item._rev,
+							...item,
+							price: itemPrice === emptyPrice ? undefined : itemPrice,
 							name: itemName,
-							state: itemState,
 							created: item.created,
 							updated: new Date(),
 						}
-						console.log(newItem)
 						saveItemEdits(item._id, newItem)
 						close()
 					}}
